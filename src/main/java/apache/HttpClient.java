44ea9368840be2ec010c1b98c202953ccde81ff5
yuanpengxing <274777453@qq.com>
2020-09-10 16:31:24 +0800

    Add HttpsClient

--- src/main/java/apache/HttpClient.java
+++ src/main/java/apache/HttpClient.java
@@ -1,3 +1,5 @@
+package apache;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
@@ -33,17 +35,10 @@ import java.security.cert.X509Certificate;
 import java.util.*;
 
 public class HttpClient {
-    private static PoolingHttpClientConnectionManager connMgr;
     private static RequestConfig requestConfig;
     private static final int MAX_TIMEOUT = 10000;
 
     static {
-        // 设置连接池
-        connMgr = new PoolingHttpClientConnectionManager();
-        // 设置连接池大小
-        connMgr.setMaxTotal(100);
-        connMgr.setDefaultMaxPerRoute(connMgr.getMaxTotal());
-
         RequestConfig.Builder configBuilder = RequestConfig.custom();
         configBuilder.setConnectTimeout(MAX_TIMEOUT);
         configBuilder.setSocketTimeout(MAX_TIMEOUT);
@@ -88,41 +83,6 @@ public class HttpClient {
     }
 
 
-    /**
-     * 发送 POST 请求（HTTP），K-V形式
-     * @param params 参数map
-     */
-    public static String postSSL(String url, Map<String, Object> params) {
-        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(createSSLConnSocketFactory()).setConnectionManager(connMgr).setDefaultRequestConfig(requestConfig).build();
-        CloseableHttpResponse response = null;
-        String result = null;
-        HttpPost httpPost = new HttpPost(url);
-        httpPost.setConfig(requestConfig);
-
-        List<NameValuePair> nvpl = new ArrayList<>(params.size());
-        for (Map.Entry<String, Object> entry : params.entrySet()) {
-            NameValuePair nvp = new BasicNameValuePair(entry.getKey(), entry.getValue().toString());
-            nvpl.add(nvp);
-        }
-
-        httpPost.setEntity(new UrlEncodedFormEntity(nvpl, Charset.forName("UTF-8")));
-        try {
-            response = httpClient.execute(httpPost);
-            HttpEntity entity = response.getEntity();
-            result = EntityUtils.toString(entity, "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (response != null) {
-                try {
-                    EntityUtils.consume(response.getEntity());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return result;
-    }
 
     /**
      * 发送 POST 请求（HTTP），JSON形式
@@ -226,74 +186,6 @@ public class HttpClient {
             }
         }
         return result;
-    }
-
-    /**
-     * 创建SSL安全连接
-     *
-     * @return
-     */
-    private static SSLConnectionSocketFactory createSSLConnSocketFactory() {
-        SSLConnectionSocketFactory sslsf = null;
-        try {
-            X509TrustManager trustManager = new X509TrustManager() {
-                @Override
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-                @Override
-                public void checkClientTrusted(X509Certificate[] xcs, String str) {}
-                @Override
-                public void checkServerTrusted(X509Certificate[] xcs, String str) {}
-            };
-            SSLContext ctx = SSLContext.getInstance(SSLConnectionSocketFactory.TLS);
-            ctx.init(null, new TrustManager[] { trustManager }, null);
-            sslsf = new SSLConnectionSocketFactory(ctx, NoopHostnameVerifier.INSTANCE);
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        }
-        return sslsf;
-    }
-
-    private static CloseableHttpClient sslClient() {
-        try {
-            // 在调用SSL之前需要重写验证方法，取消检测SSL
-            X509TrustManager trustManager = new X509TrustManager() {
-                @Override
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-                @Override
-                public void checkClientTrusted(X509Certificate[] xcs, String str) {
-                }
-                @Override
-                public void checkServerTrusted(X509Certificate[] xcs, String str) {
-                }
-            };
-            SSLContext ctx = SSLContext.getInstance(SSLConnectionSocketFactory.TLS);
-            ctx.init(null, new TrustManager[]{trustManager}, null);
-            SSLConnectionSocketFactory socketFactory =
-                    new SSLConnectionSocketFactory(ctx, NoopHostnameVerifier.INSTANCE);
-            // 创建Registry
-            RequestConfig requestConfig = RequestConfig.custom()
-                    .setCookieSpec(CookieSpecs.STANDARD_STRICT)
-                    .setExpectContinueEnabled(Boolean.TRUE)
-                    .setTargetPreferredAuthSchemes(Arrays.asList(AuthSchemes.NTLM, AuthSchemes.DIGEST))
-                    .setProxyPreferredAuthSchemes(Arrays.asList(AuthSchemes.BASIC))
-                    .build();
-            Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
-                    .register("http", PlainConnectionSocketFactory.INSTANCE)
-                    .register("https", socketFactory).build();
-            // 创建ConnectionManager，添加Connection配置信息
-            PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
-            CloseableHttpClient closeableHttpClient = HttpClients.custom()
-                    .setConnectionManager(connectionManager)
-                    .setDefaultRequestConfig(requestConfig)
-                    .build();
-            return closeableHttpClient;
-        } catch (KeyManagementException | NoSuchAlgorithmException ex) {
-            throw new RuntimeException(ex);
-        }
     }
 
 
---
+++ src/main/java/apache/HttpsClient.java
@@ -0,0 +1,127 @@
+package apache;
+
+import org.apache.http.client.config.AuthSchemes;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.*;
+
+public class HttpsClient {
+    private static RequestConfig requestConfig;
+    private static int MAX_TIMEOUT = 10000;
+
+    static {
+        RequestConfig.Builder configBuilder = RequestConfig.custom();
+        configBuilder.setCookieSpec(CookieSpecs.STANDARD_STRICT);
+        configBuilder.setExpectContinueEnabled(Boolean.TRUE);
+        configBuilder.setTargetPreferredAuthSchemes(Arrays.asList(AuthSchemes.NTLM, AuthSchemes.DIGEST));
+        configBuilder.setProxyPreferredAuthSchemes(Arrays.asList(AuthSchemes.BASIC));
+        configBuilder.setConnectTimeout(MAX_TIMEOUT);
+        configBuilder.setSocketTimeout(MAX_TIMEOUT);
+        configBuilder.setConnectionRequestTimeout(MAX_TIMEOUT);
+        requestConfig = configBuilder.build();
+    }
+
+    private static CloseableHttpClient closeableHttpClient() {
+        SSLConnectionSocketFactory socketFactory = createSSLConnSocketFactory();
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.INSTANCE)
+                .register("https", socketFactory).build();
+        // 创建ConnectionManager，添加Connection配置信息
+        PoolingHttpClientConnectionManager connMgr = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        connMgr.setMaxTotal(100);
+        connMgr.setDefaultMaxPerRoute(connMgr.getMaxTotal());
+        CloseableHttpClient closeableHttpClient = HttpClients.custom()
+                .setConnectionManager(connMgr)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+        return closeableHttpClient;
+    }
+
+    public static String post(String url, String postJson) {
+        CloseableHttpClient httpClient = closeableHttpClient();
+
+        CloseableHttpResponse response = null;
+        HttpPost httpPost = new HttpPost(url);
+        String result = null;
+
+        httpPost.setHeader("Content-Type", "application/json;charset=utf-8");
+        httpPost.setEntity(new StringEntity(postJson, "utf-8"));
+        try {
+            response = httpClient.execute(httpPost);
+            result = EntityUtils.toString(response.getEntity(), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (response != null) {
+                try {
+                    EntityUtils.consume(response.getEntity());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+
+    }
+
+    private static SSLConnectionSocketFactory createSSLConnSocketFactory() {
+        SSLConnectionSocketFactory sslsf = null;
+        try {
+            X509TrustManager trustManager = new X509TrustManager() {
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] xcs, String str) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] xcs, String str) {
+                }
+            };
+            SSLContext ctx = SSLContext.getInstance(SSLConnectionSocketFactory.TLS);
+            ctx.init(null, new TrustManager[]{trustManager}, null);
+            sslsf = new SSLConnectionSocketFactory(ctx, NoopHostnameVerifier.INSTANCE);
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return sslsf;
+    }
+
+    public static void main(String[] args) {
+        String requestParams = "{\n" +
+                "\"callbackType\":\"1\",\n" +
+                "\"callbackUri\":\"http://121.36.111.187:44597/VM-ALARM/alarm/receive/receiveAlarm\",\n" +
+                "\"channelId\":\"5\",\n" +
+                "\"endSubsTime\":\"1593532800000\",\n" +
+                "\"resourceType\":35,\n" +
+                "\"isValid\":true\n" +
+                "}";
+        String result = post("https://117.78.52.95:443/vas/api/v2/info/subscribe", requestParams);
+        System.out.println(result);
+    }
+
+}
